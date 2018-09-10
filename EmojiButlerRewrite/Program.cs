3e321c534198047ee7cb4f8c41e99b08f96dd472
ExtraConcentratedJuice <onomatopoeicboom@gmail.com>
2018-09-10 16:41:29 -0700

    Add project files.

---
+++ EmojiButlerRewrite/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmojiButlerRewrite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
